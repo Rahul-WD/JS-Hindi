7ab776b77c9c0e54117c0f6bb601d86aeaf4db16
Rahul Chauhan <rahul.webdeveloper01@gmail.com>
2025-09-17 09:51:23 +0000

    added 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS codes as a newer version.
+//alert (3 + 3) // we ase using node.js not any browser.
+
+console.log(3 
+    +
+      3) //code readablities should be high
+
+
+      let name = "Rahul"
+      let age = 21
+      let isLoggedIn = true;
+      let country;
+
+/* DataTypes
+    number = 2 to power 53
+    bigint
+    string = ""
+    boolean = true/false
+    null = standalone value
+    undefined = 
+    symbol = unique
+*/
+
+
+// Objects
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
+
